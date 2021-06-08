309ac2b0c9542f0d82f4b899d85221dc97e49647
Osama Shamim <osamashamim1997@gmail.com>
2021-06-08 22:23:06 +0500

    mern app

---
+++ Client/dashboard/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/login.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
